f0ea81320d3d8213b000b789bcf2f9789c867ca4
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2023-09-27 12:52:48 +0530

    Added Object

--- Image Search App/index.js
+++ Image Search App/index.js
@@ -3,51 +3,85 @@ const accessKey = 'RjDaqrj1o6CP9OKi6r5xfu7DKASKFKRAyUdnJQBthAs';
 const searchForm = document.querySelector('form');
 const imagesContainer = document.querySelector('.images-container');
 const searchInput = document.querySelector('.search-input');
+const loadMoreBtn = document.querySelector('.loadMoreBtn');
 
 
 
 // Function to fetch images using Unsplash API
 
-const fetchImages = async (query) => {
-      imagesContainer.innerHTML = '';
+const fetchImages = async (query, pageNo) => {
+      
+      try {
 
+      if (pageNo === 1) {
+            imagesContainer.innerHTML = '';  
+      }
 
-      const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&client_id=${accessKey}`;  
+      const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&page=${pageNo}&client_id=${accessKey}`;
+
 
       const response = await fetch(url);
       const data = await response.json();
       
-      data.results.forEach(photo => {
-            // Creading Image Div
-            const imageElement = document.createElement('div');
-            imageElement.classList.add('imageDiv');
-            imageElement.innerHTML = `<img src=${photo.urls.regular}/>`;
-
-            // creating overlay 
-            const overlayElement = document.createElement('div');
-            overlayElement.classList.add('overlay');
-
-            // Creating overlay Text
-            const overlayText = document.createElement('h3');
-            overlayText.innerText = `${photo.alt_description}` 
-
-            overlayElement.appendChild(overlayText);
-            imageElement.appendChild(overlayElement);
-            imagesContainer.appendChild(imageElement);
-      });
-};
-
+      if (data.results.length > 0) {
+            data.results.forEach(photo => {
+                  // Creading Image Div
+                  const imageElement = document.createElement('div');
+                  imageElement.classList.add('imageDiv');
+                  imageElement.innerHTML = `<img src=${photo.urls.regular}/>`;
+      
+                  // creating overlay 
+                  const overlayElement = document.createElement('div');
+                  overlayElement.classList.add('overlay');
+      
+                  // Creating overlay Text
+                  const overlayText = document.createElement('h3');
+                  overlayText.innerText = `${photo.alt_description}` 
+      
+                  overlayElement.appendChild(overlayText);
+                  imageElement.appendChild(overlayElement);
+                  imagesContainer.appendChild(imageElement);
+            });
+      
+            if (data.total_pages === pageNo) {
+                  loadMoreBtn.style.display = "none";
+            }
+            else {
+                  loadMoreBtn.style.display = "block";
+            }
+      }
+      else {
+            imagesContainer.innerHTML = `<h2>No image found</h2>`;
+            if (loadMoreBtn.style.display === "block") 
+                  loadMoreBtn.style.display = "none";
+            }
+      }
+      catch (error) {
+            imagesContainer.innerHTML = `<h2>failed to fetch images. Please try again later.</h2>`  
+      }
+}
 // fetchImages();
 
 // adding Event Listener to search form
+
+let page = 1;
 
 searchForm.addEventListener('submit', (e) => {
       e.preventDefault();
       const inputText = searchInput.value.trim();
       if (inputText !== '') {
-            fetchImages(inputText);
+            page = 1;
+            fetchImages(inputText,page);
       }
       else {
-            imagesContainer.innerHTML = `<h2>Please enter a search query.</h2>`
+            imagesContainer.innerHTML = `<h2>Please enter a search query.</h2>`;
+            if (loadMoreBtn.style.display === "block") 
+                  loadMoreBtn.style.display = "none";
       }
+});
+
+// adding Event Listener to load more button to fetch more images
+
+loadMoreBtn.addEventListener("click", () => {
+      fetchImages(searchInput.value.trim(),++page);   
 });
